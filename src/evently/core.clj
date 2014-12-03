a0a38344fd8f0d533d5f08203ba66947e7398f9f
Mattias Holmqvist <mattias.holmqvist@gmail.com>
2014-12-03 14:46:10 +0100

    Added materialize function

--- src/evently/core.clj
+++ src/evently/core.clj
@@ -1,5 +1,8 @@
 (ns evently.core)
 
+(defn safe-inc [x]
+  ((fnil inc 0) x))
+
 (defn aggregate-id? "Returns true if the id is a valid aggregate id"
   [id]
   (string? id))
@@ -13,7 +16,8 @@
     (or
       (empty? (:uncommitted-events ar))
       (seq? (:uncommitted-events ar))))
-    (pos? (:version ar))
+    (or (nil? (:version ar))
+        (pos? (:version ar)))
     (associative? (:state ar)))
 
 (defn aggregate
@@ -24,7 +28,7 @@
   {:id aggregate-id
    :type type
    :uncommitted-events []
-   :version 1
+   :version 0
    :state {}})
 
 (defn events
@@ -70,9 +74,11 @@
     (associative? (:data e))))
 
 (defn next-version
-  "Returns the next expected version of the given aggregate"
+  "Returns the next expected version of the given aggregate. Returns 0 if the
+  aggregate does not yet have a version."
   [aggregate-root]
-  ((fnil inc 0) (:version aggregate-root)))
+  {:aggregate-id (:id aggregate-root)
+   :version (safe-inc (:version aggregate-root))})
 
 (defn make-event
   "Generates an event from the given input. event-id should be a valid id,
@@ -84,15 +90,33 @@
    :type type
    :data data})
 
+(defn- apply-metadata-from [aggregate-root event]
+  (-> aggregate-root
+    (assoc-in [:version] (:version event))
+    (assoc-in [:timestamp] (:timestamp event))))
+
+(defn- apply-state-from [aggregate-root event]
+  (update-in aggregate-root [:state] handle-event event))
+
 (defn apply-change
   "Generates a new version of the aggregate root with the event applied."
   [aggregate-root event]
   {:pre  [(aggregate-root? aggregate-root)
           (event? event)]}
-  (let [version (next-version aggregate-root)
-        versioned-event (assoc event :version version)]
+  (let [event-metadata (next-version aggregate-root)
+        versioned-event (merge event event-metadata)]
     (-> aggregate-root
-      (assoc-in [:version] (:version versioned-event))
-      (assoc-in  [:timestamp] (:timestamp versioned-event))
-      (update-in [:uncommitted-events] conj event)
-      (update-in [:state] handle-event versioned-event))))
+      (apply-metadata-from versioned-event)
+      (apply-state-from versioned-event)
+      (update-in [:uncommitted-events] conj versioned-event))))
+
+(defn replay-event
+  [aggregate-root event]
+  (-> aggregate-root
+    (apply-metadata-from event)
+    (apply-state-from event)))
+
+(defn materialize [events type]
+  (let [aggregate-id (:aggregate-id (first events))
+        root (aggregate aggregate-id type)]
+    (reduce replay-event root events)))
--- test/evently/core_test.clj
+++ test/evently/core_test.clj
@@ -14,13 +14,14 @@
 
 (deftest apply-two-events-test
   (let [last-timestamp 10
-        ar (-> "some-id"
+        aggregate-id "some-id"
+        ar (-> aggregate-id
            (aggregate :thing)
            (apply-change (make-event "event-1" 1 :thing-created {}))
            (apply-change (make-event "event-2" last-timestamp :something-happened-to-thing {:amount 20})))]
 
   (testing "Adding two events increments aggregate version with two"
-    (is (= 3 (version ar))))
+    (is (= 2 (version ar))))
 
   (testing "Retrieves the events in insertion-order"
     (is (= "event-1" (:event-id (first  (events ar)))))
@@ -31,3 +32,22 @@
 
   (testing "Aggregate root state updates is configurable by injected handlers"
     (is (= {:amount 20} (state ar)))))))
+
+(deftest materialize-test
+  (let [aggregate-id "ar-1"
+        uncommitted (->
+                      (aggregate aggregate-id :thing)
+                      (apply-change (make-event "event-1" 1 :thing-created {}))
+                      (apply-change (make-event "event-2" 2 :something-happened-to-thing {:state-key :first-value}))
+                      events)
+        aggregate (materialize uncommitted :thing)
+
+        overwritten-aggregate (-> aggregate
+                                  (apply-change (make-event "event-3" 2 :something-happened-to-thing {:state-key :overwritten-value}))
+                                  events
+                                  (materialize :thing))]
+  (testing "Materializing aggregate from events"
+    (is (= 0 (count (events aggregate))))
+    (is (= 2 (version aggregate)))
+    (is (= {:state-key :first-value} (state aggregate)))
+    (is (= {:state-key :overwritten-value} (state overwritten-aggregate))))))
