92394e6f7803eb33214cbf0316856c66aa7e0968
Mattias Holmqvist <mattias.holmqvist@gmail.com>
2016-05-25 20:37:47 +0200

    Bumped version and changed repo to releases

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject evently "0.1.3"
+(defproject evently "0.1.4-SNAPSHOT"
   :description "Small event sourcing library for Clojure"
   :url "http://github.com/mattiasholmqvist/evently"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :deploy-repositories [["clojars" {:url "https://clojars.org/evently" :creds :gpg}]])
+  :deploy-repositories [["releases" {:url "https://clojars.org/evently" :creds :gpg}]])
