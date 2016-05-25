a212408a40ed24c96acc2ce915327477cb387219
Mattias Holmqvist <mattias.holmqvist@gmail.com>
2016-05-25 20:34:28 +0200

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject evently "0.1.2-SNAPSHOT"
+(defproject evently "0.1.2"
   :description "Small event sourcing library for Clojure"
   :url "http://github.com/mattiasholmqvist/evently"
   :license {:name "Eclipse Public License"
