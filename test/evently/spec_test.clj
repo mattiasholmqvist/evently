ee77b2441d02c257f0a89f2b27a38ee0f97eab22
Ben Sima <bensima@gmail.com>
2016-06-29 19:12:23 -0700

    Add spec tests
    Some of them are just stubbed for now. I'll have to work on figuring out
    how to fix the specs so that the commented-out tests pass.

---
+++ test/evently/spec_test.clj
@@ -0,0 +1,66 @@
+(ns evently.spec-test
+  (:require [clojure
+             [spec :as spec]
+             [test :as t :refer [deftest is]]]
+            [clojure.spec.test :as s]
+            [evently.core :as e]))
+
+(def ^:dynamic *num-tests* 15)
+
+(defn spec-test
+  "A helper for running tests with clojure.spec.test"
+  [sym]
+  (-> sym
+    (s/check-var :num-tests *num-tests*)
+    :result
+    true?))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest safe-inc
+  (is (spec-test #'evently.core/safe-inc)))
+
+(deftest aggregate-spec
+  (is (spec-test #'evently.core/aggregate)))
+
+(deftest events-spec
+  (is (spec-test #'evently.core/events)))
+
+(deftest version-spec
+  (is (spec-test #'evently.core/version)))
+
+(deftest timestamp-spec
+  (is (spec-test #'evently.core/timestamp)))
+
+(deftest state-spec
+  (is (spec-test #'evently.core/state)))
+
+(deftest event-dispatcher-spec
+  (is (spec-test #'evently.core/event-dispatcher)))
+
+(deftest next-version-spec
+  (is (spec-test #'evently.core/next-version)))
+
+(deftest make-event-spec
+  (is (spec-test #'evently.core/make-event)))
+
+;;; TODO
+
+#_(deftest apply-metadata-from-spec
+  (is (spec-test #'evently.core/apply-metadata-from)))
+
+#_(deftest apply-state-from-spec
+  (is (spec-test #'evently.core/apply-state-from)))
+
+#_(deftest apply-change-spec
+  (is (spec-test #'evently.core/apply-change)))
+
+#_(deftest replay-event-spec
+  (is (spec-test #'evently.core/replay-event)))
+
+#_(deftest materialze-spec
+  (is (spec-test #'evently.core/materialize)))
+
+#_(deftest emit-event-spec
+  (is (spec-test #'evently.core/emit-event)))
+
