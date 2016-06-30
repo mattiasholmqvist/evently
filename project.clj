cb93d3d0467fcf1eb18118b4b6e57478755782a2
Ben Sima <bensima@gmail.com>
2016-06-29 18:17:38 -0700

    Add test.check to dev dependencies
    Necessary for clojure.spec.test

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha7"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :deploy-repositories [["releases" {:url "https://clojars.org/evently" :creds :gpg}]])
