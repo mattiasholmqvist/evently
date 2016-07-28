9175d16625742d79898cc12f3183dd131a9bcfe3
Ben Sima <bensima@gmail.com>
2016-07-27 17:55:36 -0700

    Condense spec tests

--- test/evently/spec_test.clj
+++ test/evently/spec_test.clj
@@ -2,72 +2,13 @@
   (:require [clojure
              [spec :as spec]
              [test :as t :refer [deftest is]]]
-            [clojure.spec.test :as s]
+            [clojure.spec.test :as stest]
             [evently.core :as e]))
 
 (def ^:dynamic *num-tests* 15)
 
-(deftest all-spec
-  (let [r (s/test)]
-    (println r)
-    (is (true? (:result r)))))
-
-(comment
-  (defn spec-test
-    "A helper for running tests with clojure.spec.test"
-    [sym]
-    (-> sym
-      (s/test {:num-tests *num-tests*})
-      :fail
-      zero?))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-  (deftest safe-inc
-    (is (spec-test #'evently.core/safe-inc)))
-
-  (deftest aggregate-spec
-    (is (spec-test #'evently.core/aggregate)))
-
-  (deftest events-spec
-    (is (spec-test #'evently.core/events)))
-
-  (deftest version-spec
-    (is (spec-test #'evently.core/version)))
-
-  (deftest timestamp-spec
-    (is (spec-test #'evently.core/timestamp)))
-
-  (deftest state-spec
-    (is (spec-test #'evently.core/state)))
-
-  (deftest event-dispatcher-spec
-    (is (spec-test #'evently.core/event-dispatcher)))
-
-  (deftest next-version-spec
-    (is (spec-test #'evently.core/next-version)))
-
-  (deftest make-event-spec
-    (is (spec-test #'evently.core/make-event)))
-
-;;; TODO
-
-  #_(deftest apply-metadata-from-spec
-      (is (spec-test #'evently.core/apply-metadata-from)))
-
-  #_(deftest apply-state-from-spec
-      (is (spec-test #'evently.core/apply-state-from)))
-
-  #_(deftest apply-change-spec
-      (is (spec-test #'evently.core/apply-change)))
-
-  #_(deftest replay-event-spec
-      (is (spec-test #'evently.core/replay-event)))
-
-  #_(deftest materialze-spec
-      (is (spec-test #'evently.core/materialize)))
-
-  #_(deftest emit-event-spec
-      (is (spec-test #'evently.core/emit-event)))
-
-  )
+(deftest specs
+  (-> 'evently.core
+    stest/enumerate-namespace
+    (stest/check {:num-tests *num-tests*})
+    stest/summarize-results))
